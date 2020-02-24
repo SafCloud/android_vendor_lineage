d5021b1e35e8c9d6971721f5f8919cbce3e5266a
srhughes <46631343+SafCloud@users.noreply.github.com>
2020-02-24 14:12:51 -0500

    Updated Packages

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -3,7 +3,20 @@ $(call inherit-product, vendor/lineage/config/common_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
-    LatinIME
+    LatinIME \
+    Jelly
+    
+# SafOS packages
+PRODUCT_PACKAGES += \
+    Mail \
+    OpenWeatherMapWeatherProvider \
+    AccountManager \
+    MagicEarth \
+    Camera \
+    eDrive \
+    Weather
+    Tasks 
+
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
