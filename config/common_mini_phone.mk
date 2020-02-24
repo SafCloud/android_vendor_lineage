7640b32bce3f7cfa995cbbe0e795f1ff523ee993
srhughes <46631343+SafCloud@users.noreply.github.com>
2020-02-24 14:13:44 -0500

    Update common_mini_phone.mk

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -4,5 +4,12 @@ $(call inherit-product, vendor/lineage/config/common_mini.mk)
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
-
+    
+# SafOS packages
+PRODUCT_PACKAGES += \
+    OpenWeatherMapWeatherProvider \
+    AccountManager \
+    eDrive \
+    Weather
+    
 $(call inherit-product, vendor/lineage/config/telephony.mk)
